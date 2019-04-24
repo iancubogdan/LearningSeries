a97bf375db586a56e81a84b95af631acbb360a5b
biancu <biancu@deloittece.com>
2019-04-24 12:15:31 +0300

    spring initialzr

---
+++ src/main/java/com/java/learningSeries/LearningSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.java.learningSeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSeriesApplication.class, args);
+	}
+
+}
